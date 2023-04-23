2d8085811a9e1f503efba371c6e462b00cf1c2b8
Alexander <formysite.ru@yandex.ru>
2023-04-23 18:02:17 +0300

    not css

--- hmobjarr.js
+++ hmobjarr.js
@@ -1,1 +1,95 @@
 'use strict';
+
+// let hello=['Привет, ', 'мир', '!'];
+// alert(hello[0] + hello[1] + hello[2]);
+
+// let hl=['Привет ', 'мир', '!'];
+// let text = hl[0] + hl[1] + hl[2];
+// alert(text);
+
+// let hi=['Привет ', 'мир' , '!'];
+//     hi[0]='Здравствуй, ';
+// alert(hi);
+
+// let obj={'Николай':'100' , 'Сергей':'200' , 'Василий':'300' , 'Петр':'500'};
+// alert(obj['Николай']);
+// alert(obj['Петр']);
+
+// let arr=[1, 2, 3, 4, 5];
+
+// let arr1=[];
+//     arr1[0] = 1;
+//     arr1[1] = 2;
+//     arr1[2] = 3;
+//     arr1[3] = 4;
+//     arr1[4] = 5;
+// alert(arr);
+// alert(arr1);
+
+
+// let arr = {
+//     'ru':['голубой', 'красный', 'зеленый'],
+//     'en':['blue', 'red', 'green']
+// };
+// alert(arr['ru'][0]);
+
+// let arr = ['a' , 'b' , 'c'];
+// alert(arr);
+
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+
+// let arr =['a' , 'b' , 'c' , 'd'];
+// alert(arr[0]+ '+' + arr[1] + ', ' + arr[2] + '+' +arr[3]);
+
+// let arr=[2, 5, 3, 9];
+// let arr1=arr[0]*arr[1];
+// let arr2=arr[2]*arr[3];
+// alert(arr1);
+// alert(arr2);
+// let result=arr1+arr2;
+// alert(result);
+
+
+// let obj = {a: 1, b: 2, c: 3};
+// alert(obj['a']);
+// alert(obj.a);
+
+// let obj = {Коля: '1000', Вася: '500', Петя: '200'};
+// alert(obj.Коля);
+// alert(obj.Петя);
+
+
+// let week={первый:'Понедельник', второй:'Вторник', третий:'Среда', четвертый:'Четверг', пятый:'Пятница', шестой:'Суббота', седьмой:'Воскресенье'};
+// alert(week.седьмой);
+
+// let week={1:'Понедельник', 2:'Вторник', 3:'Среда', 4:'Четверг', 5:'Пятница', 6:'Суббота', 7:'Воскресенье'};
+// let day = week[3];
+// alert(day);
+
+// let arr =[
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// alert(arr['1'][0]);
+
+// let obj={js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// alert(obj.js[0]);
+
+// let arr={
+//     'ru':['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en':['Monday', 'Tuesday', 'Wedenday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// };
+// alert(arr['ru'][0]);
+// alert(arr['en'][2]);
+
+
+// let arr={
+//     'ru':{1:'Понедельник', 2:'Вторник', 3:'Среда', 4:'Четверг', 5:'Пятница', 6:'Суббота', 7:'Воскресенье'},
+//     'en':{1:'Monday', 2:'Tuesday', 3:'Wedenday', 4:'Thursday', 5:'Friday', 6:'Saturday', 7:'Sunday'}
+// };
+// let lang = prompt('Выбирите язык ru или en', ' ');
+// let day = prompt('Укажите день недели цифрой', ' ');
+// alert(arr[lang][day]);
